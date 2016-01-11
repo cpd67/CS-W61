0e1d7e29eaaaf02b10d3bfc8043bbe64f58f8ed5
abs25 <abs25@students.calvin.edu>
2016-01-11 14:51:01 -0500

    added user input functionality

--- Interim Final Project/src/edu/calvin/csw61/finalproject/Driver.java
+++ Interim Final Project/src/edu/calvin/csw61/finalproject/Driver.java
@@ -1,9 +1,22 @@
 package edu.calvin.csw61.finalproject;
 
+import java.util.Scanner;
+
 public class Driver {
 
 	public static void main(String[] args) {
+		Player p = new Player();
 		System.out.println("Welcome to Lost in Knightdale!");
+		
+		//infinite loop
+		while(true){
+			//new scanner to get user input
+			Scanner scanner = new Scanner(System.in);
+			String command = scanner.nextLine();
+			
+			//execute the command
+			p.executeCommand(command);
+		}
 	}
 
 }
