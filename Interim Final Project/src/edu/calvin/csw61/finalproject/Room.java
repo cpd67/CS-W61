92667493d9e812c1a15b38387daf1b4249770fb0
Chris <codemonkey941@gmail.com>
2016-01-16 16:55:30 -0500

    Walk finally works! Created documentation for making a map, added static HashMaps for all four buildings, and tried to implement Take.

--- Interim Final Project/src/edu/calvin/csw61/finalproject/Room.java
+++ Interim Final Project/src/edu/calvin/csw61/finalproject/Room.java
@@ -1,13 +1,21 @@
 package edu.calvin.csw61.finalproject;
 
+import java.util.ArrayList;
+
 public class Room {
+	
 	private String myDescriptor;
 	private ObjectInterface[] myObjects;
 	private boolean myNorthDoor, mySouthDoor, myEastDoor, myWestDoor, hasNPC, hasMonster, playerPresent;
 	//bordering rooms
 	private ObjectInterface myMonster;
 	private ObjectInterface myNPC;
-	
+	private ArrayList<ApertureBehavior> myApertures;
+	private Integer[] myNextRoomNumbers;  //Store a reference to the next Room number
+	//myNextRoomNumbers = {North Room, South Room, East Room, West Room};
+	//No room = -1 in the slot.
+	//Outside = -2
+		
 	public Room() {
 		myObjects = new ObjectInterface[2];
 		myNorthDoor = mySouthDoor = myEastDoor = myWestDoor = hasNPC = hasMonster = false;
@@ -15,13 +23,23 @@ public class Room {
 		playerPresent = false;
 		hasNPC = false;
 		hasMonster = false;
+		myApertures = new ArrayList<ApertureBehavior>(); //List of Apertures
+		myNextRoomNumbers = new Integer[4]; //Four Doors = four possible Rooms
 	}
 	
 	//explicit constructor
-	public Room(boolean north, boolean south, boolean east, boolean west, boolean needNPC, boolean needMonster){
-		setDoors(north, south, east, west);
+	//If no Doors, then the Room will have the NextRoomNumber as itself (it's own Room number in the HashMap of Rooms).
+	public Room(boolean north, boolean south, boolean east, boolean west, boolean needNPC, boolean needMonster, Integer[] nextRooms){
+		myNorthDoor = north;
+		mySouthDoor = south;
+		myEastDoor = east;
+		myWestDoor = west;
+		myApertures = new ArrayList<ApertureBehavior>(); //List of Apertures
+		myNextRoomNumbers = nextRooms;
+		setDoors();
 		hasNPC = needNPC;  //Do I need an NPC? 
 		hasMonster = needMonster;  //How about a Monster?
+		myObjects = new ObjectInterface[2];  //I can store Objects
 	}
 	
 	public String getDescriptor() {
@@ -33,11 +51,26 @@ public class Room {
 	}
 
 	//setDoors
-	public void setDoors(boolean north, boolean south, boolean east, boolean west){
-		myNorthDoor = north;
-		mySouthDoor = south;
-		myEastDoor = east;
-		myWestDoor = west;
+	public void setDoors(){
+		if(myNorthDoor) { //North
+			ApertureBehavior northDoor = new Door(this, myNextRoomNumbers[0], false, false, "north");
+			myApertures.add(northDoor);
+		} 
+		
+		if(mySouthDoor) {  //South
+			ApertureBehavior southDoor = new Door(this, myNextRoomNumbers[1], false, false, "south");
+			myApertures.add(southDoor);
+		} 
+		
+		if(myEastDoor) {  //East
+			ApertureBehavior eastDoor = new Door(this, myNextRoomNumbers[2], false, false, "east");
+			myApertures.add(eastDoor);
+		}
+		
+		if(myWestDoor) {  //West
+			ApertureBehavior westDoor = new Door(this, myNextRoomNumbers[3], false, false, "west");
+			myApertures.add(westDoor);
+		}
 	}
 	
 	//setObjects
@@ -84,5 +117,29 @@ public class Room {
 		return null; //No Monster otherwise
 	}
 	
+	//Get myObjects (If I have any)
+	public ObjectInterface[] getObjects() {
+		return myObjects;
+	}
+	
+	//Is the Player in the Room?
+	public boolean hasPlayer() {
+		return playerPresent;
+	}
+	
+	//The Player is no longer in the Room.
+	public void setNoPlayer() {
+		playerPresent = false; 
+	}
+	
+	//Get the Apertures in the Room
+	public ArrayList<ApertureBehavior> getAperatures() {
+		return myApertures;
+	}
+	
+	//Take an Object from the Room (if the Player takes one).
+	public void removeObject(int number) {
+		myObjects[number] = null; //Make it null, check if it's null later.
+	}
 }
 
--- Interim Final Project/src/edu/calvin/csw61/finalproject/playercommands/Walk.java
+++ Interim Final Project/src/edu/calvin/csw61/finalproject/playercommands/Walk.java
@@ -1,6 +1,8 @@
 package edu.calvin.csw61.finalproject.playercommands;
 
+import edu.calvin.csw61.finalproject.Door;
 import edu.calvin.csw61.finalproject.Player;
+import edu.calvin.csw61.finalproject.TestClass;
 
 public class Walk implements Command{
 	String myDirection;
@@ -15,6 +17,24 @@ public class Walk implements Command{
 
 	@Override
 	public void execute() {
+		//Check if we are outside...
+		for(int i = 0; i < myPlayer.getRoom().getAperatures().size(); i++) {
+			if(myPlayer.getRoom().getAperatures().get(i).getName().equals("door")) {  //If the Aperature is a Door...
+				Door checker = (Door)myPlayer.getRoom().getAperatures().get(i);  //Typecast to a Door
+				//Check if the Door is locked...
+				//Unlocked, so check if the Door's direction is equal to the Player's direction.
+				if(checker.getDir().equals(myPlayer.getDirection())) {  //Check the Direction
+					System.out.println("You walked through the door.");
+					checker.goThrough(myPlayer); //Go through the Door.
+					break; //Only go through the Door once.
+				}
+			} else {
+				//It's a Wall, can't walk there.
+				System.out.println("A wall blocks your path.");
+				break;  //For now...
+			}
+		}
+		
 		//get direction
 		//if the player's direction matches the door's direction, then go through the door
 		//(go to next spot in two dimensional array of rooms) 
